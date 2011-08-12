f2ed72d960e8ebd75beef4d630bb348ebcdf1e25
arturo <arturo@reactable.com>
2011-08-12 14:29:14 +0200

    gles2-bc: more android/iphone fixes

--- addons/ofxGLES2Renderer/libs/gles2-bc/src/OpenGLES/OpenGLES20/OpenGLES20Context.cpp
+++ addons/ofxGLES2Renderer/libs/gles2-bc/src/OpenGLES/OpenGLES20/OpenGLES20Context.cpp
@@ -14,10 +14,10 @@
  limitations under the License.
  */
 
-#if TARGET_OS_IPHONE
+#ifndef __ANDROID__
 	#include <OpenGLES/ES2/gl.h>
 	#include <OpenGLES/ES2/glext.h>
-#elif defined( __ANDROID__ )
+#else
 	#include <GLES2/gl2.h>
 	#include <GLES2/gl2ext.h>
 #endif
